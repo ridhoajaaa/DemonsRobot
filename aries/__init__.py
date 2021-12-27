f75e59b4ee52f89ac63c31f1c0ae6245ce5f82c2
ddodxy <alfariziridha014@gmail.com>
2021-12-27 13:06:57 +0800

    Update __init__.py

--- aries/__init__.py
+++ aries/__init__.py
@@ -110,7 +110,6 @@ if ENV:
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
     NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
     DEL_CMDS = bool(os.environ.get("DEL_CMDS", False))
-    CUSTOM_CMD = List[str] = ['/', '!']
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
     WORKERS = int(os.environ.get("WORKERS", 8))
     BAN_STICKER = os.environ.get("BAN_STICKER", "CAADAgADOwADPPEcAXkko5EB3YGYAg")
