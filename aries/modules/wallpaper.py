954e4d0ae62df0d90277f2ec0e93bfc6ebd0631e
ddodxy <alfariziridha014@gmail.com>
2021-12-27 13:32:27 +0800

    Update wallpaper.py

--- aries/modules/wallpaper.py
+++ aries/modules/wallpaper.py
@@ -4,9 +4,8 @@ import requests as r
 from aries import SUPPORT_CHAT, WALL_API, dispatcher
 from aries.modules.disable import DisableAbleCommandHandler
 from telegram import Update
-from telegram.ext import CallbackContext, register
 from aries import telethn as tbot
-from aries.events import register
+from aries.events import CallbackContext, register
 from aries import ubot
 
 # Wallpapers module by @TheRealPhoenix using wall.alphacoders.com
