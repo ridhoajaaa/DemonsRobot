4d794c0f206e243b23067b61fc2b66468bcae495
ddodxy <alfariziridha014@gmail.com>
2022-01-03 19:10:45 +0800

    Update sangmata.py

--- aries/modules/sangmata.py
+++ aries/modules/sangmata.py
@@ -1,56 +1,40 @@
+import datetime
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from aries import telethn as tbot
 from aries.events import register
 from aries import ubot
 from asyncio.exceptions import TimeoutError
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
 
 
 @register(pattern="^/sg ?(.*)")
-async def lastname(steal):
-    steal.pattern_match.group(1)
-    puki = await steal.reply("```Retrieving Such User Information..```")
-    if steal.fwd_from:
-        return
-    if not steal.reply_to_msg_id:
-        await puki.edit("```Please Reply To User Message.```")
-        return
-    message = await steal.get_reply_message()
+async def _(event):
+    if event.fwd_from:
+        return 
+    if not event.reply_to_msg_id:
+       await event.edit("```Reply to any user message.```")
+       return
+    reply_message = await event.get_reply_message() 
+    if not reply_message.text:
+       await event.edit("```reply to text message```")
+       return
     chat = "@SangMataInfo_bot"
-    user_id = message.sender.id
-    id = f"/search_id {user_id}"
-    if message.sender.bot:
-        await puki.edit("```Reply To Real User's Message.```")
-        return
-    await puki.edit("```Please wait...```")
-    try:
-        async with ubot.conversation(chat) as conv:
-            try:
-                msg = await conv.send_message(id)
-                r = await conv.get_response()
-                response = await conv.get_response()
-            except YouBlockedUserError:
-                await steal.reply("```Error, report to @idzeroidsupport```")
-                return
-            if r.text.startswith("Name"):
-                respond = await conv.get_response()
-                await puki.edit(f"`{r.message}`")
-                await ubot.delete_messages(
-                    conv.chat_id, [msg.id, r.id, response.id, respond.id]
-                )
-                return
-            if response.text.startswith("No records") or r.text.startswith(
-                "No records"
-            ):
-                await puki.edit(
-                    "```I Can't Find This User's Information, This User Has Never Changed His Name Before.```"
-                )
-                await ubot.delete_messages(conv.chat_id, [msg.id, r.id, response.id])
-                return
-            else:
-                respond = await conv.get_response()
-                await puki.edit(f"```{response.message}```")
-            await ubot.delete_messages(
-                conv.chat_id, [msg.id, r.id, response.id, respond.id]
-            )
-    except TimeoutError:
-        return await puki.edit("`I'm Sick Sorry...`")
+    sender = reply_message.sender
+    if reply_message.sender.bot:
+       await event.edit("```Reply to actual users message.```")
+       return
+    await event.edit("```Processing```")
+    async with borg.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
+              await borg.forward_messages(chat, reply_message)
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("```Please unblock @sangmatainfo_bot and try again```")
+              return
+          if response.text.startswith("Forward"):
+             await event.edit("```can you kindly disable your forward privacy settings for good?```")
+          else: 
+             await event.edit(f"{response.message.message}")
