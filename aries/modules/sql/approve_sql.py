91bff32d03fb199c72f165291612add13be7b774
ddodxy <alfariziridha014@gmail.com>
2022-01-06 02:33:10 +0800

    Update approve_sql.py

--- aries/modules/sql/approve_sql.py
+++ aries/modules/sql/approve_sql.py
@@ -1,14 +1,13 @@
 import threading
-
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, UnicodeText, func, distinct
+from sqlalchemy.sql.sqltypes import BigInteger
 
 from aries.modules.sql import BASE, SESSION
-
 
 class Approvals(BASE):
     __tablename__ = "approval"
     chat_id = Column(String(14), primary_key=True)
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger, primary_key=True)
 
     def __init__(self, chat_id, user_id):
         self.chat_id = str(chat_id)  # ensure string
