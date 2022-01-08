72c9a4d55b619abafe9c186a65f6419d2a844f9a
ddodxy <alfariziridha014@gmail.com>
2022-01-09 03:16:36 +0800

    Update dev.py

--- aries/modules/dev.py
+++ aries/modules/dev.py
@@ -87,90 +87,12 @@ def gitpull(update: Update, context: CallbackContext):
 
 
 @dev_plus
-@app.on_message(filters.command("reboot") & filters.user(SUDOERS))
-async def restart_(_, message):
-    response = await message.reply_text("Restarting....")
-    if await is_heroku():
-        if HEROKU_API_KEY == "" and HEROKU_APP_NAME == "":
-            await message.reply_text(
-                "<b>HEROKU APP DETECTED!</b>\n\nIn order to restart your app, you need to set up the `HEROKU_API_KEY` and `HEROKU_APP_NAME` vars respectively!</code>"
-            )
-            return
-        elif HEROKU_API_KEY == "" or HEROKU_APP_NAME == "":
-            await message.reply_text(
-                "<b>HEROKU APP DETECTED!</b>\n\n<b>Make sure to add both</b> `HEROKU_API_KEY` **and** `HEROKU_APP_NAME` <b>vars correctly in order to be able to restart remotely!</b>"
-            )
-            return
-        try:
-            served_chats = []
-            try:
-                chats = await get_active_chats()
-                for chat in chats:
-                    served_chats.append(int(chat["chat_id"]))
-            except Exception as e:
-                pass
-            for x in served_chats:
-                try:
-                    await app.send_message(
-                        x,
-                        f"{MUSIC_BOT_NAME} has just restarted herself. Sorry for the issues.\n\nStart playing after 10-15 seconds again.",
-                    )
-                    await remove_active_chat(x)
-                except Exception:
-                    pass
-            heroku3.from_key(HEROKU_API_KEY).apps()[HEROKU_APP_NAME].restart()
-            await response.edit(
-                "**Heroku Restart**\n\nReboot has been initiated successfully! Wait for 1 - 2 minutes until the bot restarts."
-            )
-            return
-        except Exception as err:
-            await response.edit(
-                "Something went wrong while initiating reboot! Please try again later or check logs for more info."
-            )
-            return
-    else:
-        served_chats = []
-        try:
-            chats = await get_active_chats()
-            for chat in chats:
-                served_chats.append(int(chat["chat_id"]))
-        except Exception as e:
-            pass
-        for x in served_chats:
-            try:
-                await app.send_message(
-                    x,
-                    f"{MUSIC_BOT_NAME} has just restarted herself. Sorry for the issues.\n\nStart playing after 10-15 seconds again.",
-                )
-                await remove_active_chat(x)
-            except Exception:
-                pass
-        A = "downloads"
-        B = "raw_files"
-        C = "cache"
-        try:
-            shutil.rmtree(A)
-            shutil.rmtree(B)
-            shutil.rmtree(C)
-        except:
-            pass
-        await asyncio.sleep(2)
-        try:
-            os.mkdir(A)
-        except:
-            pass
-        try:
-            os.mkdir(B)
-        except:
-            pass
-        try:
-            os.mkdir(C)
-        except:
-            pass
-        await response.edit(
-            "Reboot has been initiated successfully! Wait for 1 - 2 minutes until the bot restarts."
-        )
-        os.system(f"kill -9 {os.getpid()} && bash start")
+def reboot(update: Update, context: CallbackContext):
+    update.effective_message.reply_text(
+        "Starting a new instance and shutting down this one"
+    )
+
+    os.system(f"kill -9 {os.getpid()} && python3 -m aries")
 
 
 class Store:
