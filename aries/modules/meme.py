7d74afac03c3c2be38d7608f0b1d296c0b160f7d
ddodxy <alfariziridha014@gmail.com>
2022-01-10 00:16:31 +0800

    Update meme.py

--- aries/modules/meme.py
+++ aries/modules/meme.py
@@ -1,6 +1,6 @@
 from pyrogram.types.bots_and_keyboards.inline_keyboard_button import InlineKeyboardButton
 from pyrogram.types.bots_and_keyboards.inline_keyboard_markup import InlineKeyboardMarkup
-from nksama import bot
+from aries import bot
 from pyrogram import filters
 from pyrogram.types import Message
 import requests
