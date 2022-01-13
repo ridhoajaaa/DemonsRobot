691f44f45b74729755820c5f3894f8d655c597b6
ddodxy <alfariziridha014@gmail.com>
2022-01-13 19:32:01 +0800

    add snipe and banall

--- aries/modules/random_api.py
+++ aries/modules/random_api.py
@@ -14,6 +14,20 @@ async def asupan(event):
         await event.reply("`Error 404 not found...`")
 
 
+@register(pattern="^/hl ?(.*)")
+async def _(hilih):
+    kuntul = hilih.pattern_match.group(1)
+    if not kuntul:
+        await hilih.reply("Usage: /hilih <text>")
+        return
+    try:
+        resp = requests.get(f"https://tede-api.herokuapp.com/api/hilih?kata={kuntul}").json()
+        hilihnya = f"{resp['result']}"
+        return await hilih.reply(hilihnya)
+    except Exception:
+        await hilih.reply("Something went wrong LOL...")
+
+
 @register(pattern="^/chika ?(.*)")
 async def chika(event):
     try:
