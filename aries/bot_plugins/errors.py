7cdcee3872749519ae4d1c7c032b6a9ff1a46283
ddodxy <alfariziridha014@gmail.com>
2022-01-13 15:21:27 +0800

    biassalah typo

--- aries/bot_plugins/errors.py
+++ aries/bot_plugins/errors.py
@@ -2,7 +2,7 @@ import sys
 import traceback
 from functools import wraps
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
-from aries import EVENT_LOG, pbot as app
+from aries import EVENT_LOGS, pbot as app
 
 
 def split_limits(text):
