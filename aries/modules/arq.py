6d740605bdac6f6d4a798386cb11c2a37698c648
ddodxy <alfariziridha014@gmail.com>
2022-01-15 01:59:00 +0800

    refactor bans

--- aries/modules/arq.py
+++ aries/modules/arq.py
@@ -1,12 +1,9 @@
 from pyrogram import filters
 
-from aries import arq
-from aries import pbot as app
-
-ARQ_API_URL = "https://grambuilders.tech"
+from aries import BOT_USERNAME, arq, pgram
 
 
-@app.on_message(filters.command("arq"))
+@pgram.on_message(filters.command("arq"))
 async def arq_stats(_, message):
     data = await arq.stats()
     if not data.ok:
@@ -21,20 +18,22 @@ async def arq_stats(_, message):
     platform = data.platform
     python_version = data.python
     users = data.users
-    bot = data.bot
     statistics = f"""
-📡 **Uptime:** `{uptime}`
-🔘 **Requests Since Uptime:** `{requests}`
-🔘 **CPU:** `{cpu}`
-🔘 **Memory:**
-    ⇝ **Total Used:** `{server_mem}`
-    ⇝ **API:** `{api_mem}`
-🔘 **Disk:** `{disk}`
-🔘 **Platform:** `{platform}`
-🔘 **Python:** `{python_version}`
-🔘 **Users:** `{users}`
-🔘 **Bot:** {bot}
-🔘 **Address:** {ARQ_API_URL}
+**System Statistics**
+**Uptime:** `{uptime}`
+**Requests Since Uptime:** `{requests}`
+**CPU:** `{cpu}`
+**Memory:**
+    **Total Used:** `{server_mem}`
+    **API:** `{api_mem}`
+**Disk:** `{disk}`
+**Platform:** `{platform}`
+**Python:** `{python_version}`
+
+**ARQ Statistics:**
+**Users:** `{users}`
+
+**@{BOT_USERNAME} Some Modules Running On ARQ**
 **Powered By:** __@ddodxy__🔥
 """
     await message.reply_text(statistics, disable_web_page_preview=True)
