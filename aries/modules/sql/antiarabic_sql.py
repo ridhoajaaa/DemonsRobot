6e9c140a391ce241f9a0692ee6cd27ad0b448a57
ridhoajaaa <ridhoajaaa@users.noreply.github.com>
2022-01-18 19:20:00 +0000

    Automated code formatting
    Signed-off-by: idzero23 <88209926+idzero23@users.noreply.github.com>

--- aries/modules/sql/antiarabic_sql.py
+++ aries/modules/sql/antiarabic_sql.py
@@ -60,8 +60,11 @@ def set_chat_setting(chat_id: Union[int, str], setting: bool):
 
 def migrate_chat(old_chat_id, new_chat_id):
     with CHAT_LOCK:
-        chat_notes = SESSION.query(AntiArabicChatSettings).filter(
-            AntiArabicChatSettings.chat_id == str(old_chat_id)).all()
+        chat_notes = (
+            SESSION.query(AntiArabicChatSettings)
+            .filter(AntiArabicChatSettings.chat_id == str(old_chat_id))
+            .all()
+        )
         for note in chat_notes:
             note.chat_id = str(new_chat_id)
         SESSION.commit()
