c30b59da284965abc168f052eecd281beb3c7bf3
ddodxy <alfariziridha014@gmail.com>
2022-01-20 00:13:22 +0800

    ori aja udah

--- aries/modules/whatanime.py
+++ aries/modules/whatanime.py
@@ -126,42 +126,61 @@ async def whatanime(c: Client, m: Message):
                 syn = ", ".join(synonyms)
                 text += f"\n<b>Related:</b> {syn}"
 
-            if is_adult:
-                text += "\n<b>NSFW:</b> True"
-            text += f'\n<b>Similarity:</b> {(Decimal(similarity) * 100).quantize(Decimal(".01"))}%\n'
-            if episode:
-                text += f"<b>Episode:</b> {episode}"
-            text += f"\n<b>Scene Timestamp:</b> from {from_time} to {to_time}\n"
-
-            keyboard = InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            "More Info",
-                            url="https://anilist.co/anime/{}".format(anilist_id),
-                        )
-                    ]
-                ]
-            )
-
-            async def _send_preview():
-                with tempfile.NamedTemporaryFile() as file:
-                    async with session.get(match["video"]) as resp:
-                        while True:
-                            chunk = await resp.content.read(10)
-                            if not chunk:
-                                break
-                            file.write(chunk)
-                    file.seek(0)
-                    try:
-                        await m.reply_video(
-                            file.name, caption=text, reply_markup=keyboard
-                        )
-                        await reply.delete()
-                    except Exception:
-                        await reply.reply_text("Cannot send preview")
-
-            await _send_preview()
+async def whatanime(e):
+    media = e.media
+    if not media:
+        r = await e.get_reply_message()
+        media = getattr(r, "media", None)
+    if not media:
+        await e.reply("`Media required`")
+        return
+    ig = is_gif(media) or is_video(media)
+    if not is_image(media) and not ig:
+        await e.reply("`Media must be an image or gif or video`")
+        return
+    filename = "file.jpg"
+    if not ig and isinstance(media, MessageMediaDocument):
+        attribs = media.document.attributes
+        for i in attribs:
+            if isinstance(i, DocumentAttributeFilename):
+                filename = i.file_name
+                break
+    cut = await e.reply("`Downloading image..`")
+    content = await e.client.download_media(media, bytes, thumb=-1 if ig else None)
+    await cut.edit("`Searching for result..`")
+    file = memory_file(filename, content)
+    async with aiohttp.ClientSession() as session:
+        url = "https://api.trace.moe/search?anilistInfo"
+        async with session.post(url, data={"image": file}) as raw_resp0:
+            resp0 = await raw_resp0.json()
+        js0 = resp0.get("result")
+        if not js0:
+            await cut.edit("`No results found.`")
+            return
+        js0 = js0[0]
+        text = f'<b>{html.escape(js0["anilist"]["title"]["romaji"])}'
+        if js0["anilist"]["title"]["native"]:
+            text += f' ({html.escape(js0["anilist"]["title"]["native"])})'
+        text += "</b>\n"
+        if js0["episode"]:
+            text += f'<b>Episode:</b> {html.escape(str(js0["episode"]))}\n'
+        percent = round(js0["similarity"] * 100, 2)
+        text += f"<b>Similarity:</b> {percent}%\n"
+        at = re.findall(r"t=(.+)&", js0["video"])[0]
+        dt = pendulum.from_timestamp(float(at))
+        text += f"<b>At:</b> {html.escape(dt.to_time_string())}"
+        await cut.edit(text, parse_mode="html")
+        dt0 = pendulum.from_timestamp(js0["from"])
+        dt1 = pendulum.from_timestamp(js0["to"])
+        ctext = (
+            f"{html.escape(dt0.to_time_string())} - {html.escape(dt1.to_time_string())}"
+        )
+        async with session.get(js0["video"]) as raw_resp1:
+            file = memory_file("preview.mp4", await raw_resp1.read())
+        try:
+            await e.reply(ctext, file=file, parse_mode="html")
+        except FilePartsInvalidError:
+            await e.reply("`Cannot send preview.`")
 
 
 async def progress_callback(current, total, reply):
