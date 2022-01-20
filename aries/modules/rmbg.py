eb5370e2d6bef21d61a30f8fa037cf4d32aa9d99
ddodxy <alfariziridha014@gmail.com>
2022-01-20 23:28:41 +0800

    ..

--- aries/modules/rmbg.py
+++ aries/modules/rmbg.py
@@ -6,18 +6,20 @@ import requests
 from telethon import types
 from telethon.tl import functions
 
-from aries import TEMP_DOWNLOAD_DIRECTORY, telethn
+from aries import REM_BG_API_KEY, UNSCREEN_API, TEMP_DOWNLOAD_DIRECTORY
 from aries.events import register
+from aries import telethn as tbot
 
-REM_BG_API_KEY = ""
-UNSCREEN_API = ""
+
+REM_BG_API_KEY = "REM_BG_API_KEY"
+UNSCREEN_API = "UNSCREEN_API"
 
 
 async def is_register_admin(chat, user):
     if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
         return isinstance(
             (
-                await telethn(functions.channels.GetParticipantRequest(chat, user))
+                await tbot(functions.channels.GetParticipantRequest(chat, user))
             ).participant,
             (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
         )
@@ -27,23 +29,25 @@ async def is_register_admin(chat, user):
 
 @register(pattern="^/rmbg")
 async def _(event):
+    HELP_STR = "use `/rmbg` as reply to a media"
     if event.fwd_from:
         return
-    if event.is_group and not await is_register_admin(
-        event.input_chat, event.message.sender_id
-    ):
-        return
+    if event.is_group:
+        if await is_register_admin(event.input_chat, event.message.sender_id):
+            pass
+        else:
+            return
     if REM_BG_API_KEY is None:
-        await event.reply("You need API token from remove.bg to use this plugin.")
+        await event.reply("`You need API token from remove.bg to use this plugin.`")
         return False
     start = datetime.now()
     message_id = event.message.id
     if event.reply_to_msg_id:
         message_id = event.reply_to_msg_id
         reply_message = await event.get_reply_message()
-        idz = await event.reply("Processing...")
+        await event.reply("`Processing...`")
         try:
-            downloaded_file_name = await telethn.download_media(
+            downloaded_file_name = await tbot.download_media(
                 reply_message, TEMP_DOWNLOAD_DIRECTORY
             )
         except Exception as e:
@@ -53,14 +57,13 @@ async def _(event):
             output_file_name = ReTrieveFile(downloaded_file_name)
             os.remove(downloaded_file_name)
     else:
-        HELP_STR = "use `/rmbg` as reply to a media"
         await event.reply(HELP_STR)
         return
     contentType = output_file_name.headers.get("content-type")
     if "image" in contentType:
         with io.BytesIO(output_file_name.content) as remove_bg_image:
-            remove_bg_image.name = "idz.webp"
-            await telethn.send_file(
+            remove_bg_image.name = "rmbg.png"
+            await tbot.send_file(
                 event.chat_id,
                 remove_bg_image,
                 force_document=True,
@@ -73,7 +76,7 @@ async def _(event):
         await event.reply("Background Removed in {} seconds".format(ms))
     else:
         await event.reply(
-            "remove.bg API returned Errors. Please report to @idzeroidsupport\n`{}".format(
+            "remove.bg API returned Errors. Please report to @demonszxx\n`{}".format(
                 output_file_name.content.decode("UTF-8")
             )
         )
@@ -86,10 +89,11 @@ def ReTrieveFile(input_file_name):
     files = {
         "image_file": (input_file_name, open(input_file_name, "rb")),
     }
-    return requests.post(
+    r = requests.post(
         "https://api.remove.bg/v1.0/removebg",
         headers=headers,
         files=files,
         allow_redirects=True,
         stream=True,
     )
+    return r
